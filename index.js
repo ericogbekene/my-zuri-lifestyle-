f156abf50baabaa0b1e2d4f248c6058d749c6656
Eric Ogbekene <ericogbekene@icloud.com>
2022-11-02 13:35:36 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const Navbar = (
+
+    <nav>
+<h1> Seamless Stitches </h1>
+<ul>
+    
+    <li> Home </li>
+    <li> About </li>
+    <li> Contact </li>
+
+
+</ul>
+
+    </nav>
+)
+    
+ReactDom.render (Navbar, document.getElementByID("root"))
